07279154e3b293b20f78ca68ac645ffb8664707b
Johan Kok <info@johankok.nl>
2015-04-22 10:05:56 +0200

    Backported pull request #52 to v0.4.6

--- lib/gyoku/array.rb
+++ lib/gyoku/array.rb
@@ -9,11 +9,28 @@ module Gyoku
     # Translates a given +array+ to XML. Accepts the XML +key+ to add the elements to,
     # whether to +escape_xml+ and an optional Hash of +attributes+.
     def self.to_xml(array, key, escape_xml = true, attributes = {}, options = {})
-      iterate_with_xml array, attributes do |xml, item, attrs, index|
-        case item
-          when ::Hash   then xml.tag!(key, attrs) { xml << Hash.to_xml(item, options) }
-          when NilClass then xml.tag!(key, "xsi:nil" => "true")
-          else               xml.tag!(key, attrs) { xml << XMLValue.create(item, escape_xml) }
+
+      self_closing = options.delete(:self_closing)
+      unwrap = options[:unwrap] || false
+
+      iterate_with_xml array, key, attributes, options do |xml, item, attrs, index|
+        if self_closing
+          xml.tag!(key, attrs)
+        else
+          case item
+            when ::Hash       then
+              if unwrap
+                xml << Hash.to_xml(item, options)
+              else
+                xml.tag!(key, attrs) { xml << Hash.to_xml(item, options) }
+              end
+            when ::Array      then
+              xml.tag!(key, attrs) { xml << Array.to_xml(item, NESTED_ELEMENT_NAME) }
+            when NilClass     then
+              xml.tag!(key, "xsi:nil" => "true")
+            else
+              xml.tag!(key, attrs) { xml << XMLValue.create(item, escape_xml) }
+          end
         end
       end
     end
@@ -22,13 +39,29 @@ module Gyoku
 
     # Iterates over a given +array+ with a Hash of +attributes+ and yields a builder +xml+
     # instance, the current +item+, any XML +attributes+ and the current +index+.
-    def self.iterate_with_xml(array, attributes)
+    def self.iterate_with_xml(array, key, attributes, options, &block)
+
       xml = Builder::XmlMarkup.new
+      unwrap = options[:unwrap] || false
+
+      if (unwrap)
+        xml.tag!(key) { iterate_array(xml, array, attributes, &block) }
+      else
+        iterate_array(xml, array, attributes, &block)
+      end
+
+      xml.target!
+    end
+
+
+    # Iterates over a given +array+ with a Hash of +attributes+ and yields a builder +xml+
+    # instance, the current +item+, any XML +attributes+ and the current +index+.
+    def self.iterate_array(xml, array, attributes, &block)
       array.each_with_index do |item, index|
         yield xml, item, tag_attributes(attributes, index), index
       end
-      xml.target!
     end
+
 
     # Takes a Hash of +attributes+ and the +index+ for which to return attributes
     # for duplicate tags.
